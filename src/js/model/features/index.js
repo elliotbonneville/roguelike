2aab4080662ca7141281353c3cfdb6206b88867f
elliotbonneville <elliot.bonneville@gmail.com>
2018-11-23 10:26:58 -0500

    Import better

--- src/js/model/features/index.js
+++ src/js/model/features/index.js
@@ -1,10 +1,10 @@
-import { default as level } from '~/model/features/level';
-import { default as ui } from '~/model/features/ui';
-import { default as actors } from '~/model/features/actors';
-import { default as time } from '~/model/features/time';
-import { default as items } from '~/model/features/items';
-import { default as traps } from '~/model/features/traps';
-import { default as status } from '~/model/features/status';
+import level from '~/model/features/level';
+import ui from '~/model/features/ui';
+import actors from '~/model/features/actors';
+import time from '~/model/features/time';
+import items from '~/model/features/items';
+import traps from '~/model/features/traps';
+import status from '~/model/features/status';
 
 export const reducers = {
     level,
